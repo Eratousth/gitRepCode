972eb964aa7c2c3210c1a0fee9968b56edf79ead
Eratousth <4042974@g.highlineschools.org>
2021-10-19 17:27:00 -0700

    Changes have been made. - second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
     public static void main(String[] args){
 
         System.out.println("HelloWorld");
-        
+        //this is a change!
+        System.out.println("God has witnessed your sins");
+
     }
 }
