1b06d23ae1d147f58337a5f07201b76d6d484833
Eratousth <4042974@g.highlineschools.org>
2021-10-21 11:40:51 -0700

    added treasure

--- OctoberHunt.java
+++ OctoberHunt.java
@@ -1,11 +1,17 @@
 import java.util.*;
+import java.lang.Math;
+
 public class OctoberHunt {
-    static void Game(int x, int y, int min, int max){
+    static void Game(int x, int y, int min, int max, int treasure_x, int treasure_y){
         for (int i = min; i <= max; i++){
             System.out.println("");
             for (int e = min; e <= max; e++){
                 if (i == y && e == x) {
-                System.out.print(" o");
+                    if (y == treasure_y && x == treasure_x){
+                        System.out.print(" $");
+                    }else{
+                        System.out.print(" o");
+                    }
                 }else {
                     System.out.print(" *");
                 }
@@ -16,27 +22,40 @@ public class OctoberHunt {
         //Direction dictionary for w a s d
         Hashtable<String, Integer> directions = new Hashtable<String, Integer>();
 
-        //Player's beginning coordinates
-        int player_x = 5; 
-        int player_y = 5;
-
-        //Max and min values for board size and range
-        int max = 5;
-        int min = 1;
-
         //Applying values to w a s d
         directions.put("w",-1);
         directions.put("s", 1);
         directions.put("a",-1);
         directions.put("d", 1);
 
+        //Max and min values for board size and range
+        int max = 16;
+        int min = 1;
+
+        //Player's beginning coordinates
+        int player_x = max/2; 
+        int player_y = max/2;
+
+        //Treasure's coordinates
+        int treasure_x = (int)(Math.random() * max);
+        int treasure_y = (int)(Math.random() * max);
+        while(true){
+            if (treasure_x != player_x && treasure_y != player_y){
+                break;
+            }else{
+                treasure_x = (int)(Math.random() * max);
+                treasure_y = (int)(Math.random() * max);
+            }
+        }
+
         //int number = random.nextInt(max - min) + min;
         
         //Game loop
         while(true){
-            Game(player_x,player_y,min,max);
+            Game(player_x, player_y, min, max, treasure_x, treasure_y);
 
-            System.out.println("\nWhich direction would you like to go?");
+            //Character direction
+            System.out.println("\nWhich direction would you like to go?\n(" + treasure_x + ", " + treasure_y + ")");
             Scanner d = new Scanner(System.in);
             String direction = d.nextLine();
 
