c452cac26271092cfad74901ecb5418cd2f7acce
Eratousth <4042974@g.highlineschools.org>
2021-10-27 11:10:31 -0700

    further edits

--- OctoberHunt.java
+++ OctoberHunt.java
@@ -43,14 +43,13 @@ public class OctoberHunt {
 
         //Defines text colors
         String ANSI_RESET = "\u001B[0m";
-        String ANSI_BLACK = "\u001B[30m";
-        String ANSI_RED = "\u001B[31m";
+        //String ANSI_RED = "\u001B[31m";
         String ANSI_GREEN = "\u001B[32m";
         String ANSI_YELLOW = "\u001B[33m";
-        String ANSI_BLUE = "\u001B[34m";
-        String ANSI_PURPLE = "\u001B[35m";
-        String ANSI_CYAN = "\u001B[36m";
-        String ANSI_WHITE = "\u001B[37m";
+        //String ANSI_BLUE = "\u001B[34m";
+        //String ANSI_PURPLE = "\u001B[35m";
+        //String ANSI_CYAN = "\u001B[36m";
+        //String ANSI_WHITE = "\u001B[37m";
 
         boolean win = false;
 
@@ -140,7 +139,7 @@ public class OctoberHunt {
             Scanner d = new Scanner(System.in);
             String direction = d.nextLine();
 
-            if (direction.equals("e")){
+            if (direction.equals("e") || direction.equals("q") || direction.equals("y")){
                 break;
             }else if (direction.equals("w") || direction.equals("s")){
                 player_y += directions.get(direction);
